bcd66a4f916204c9eba907dbc4d5a530a485b77b
sadaf.farajian <sadaf.farajian@dayasoft.com>
2022-01-27 23:31:39 +0330

    add a default no image

--- src/components/shared/DataCards.js
+++ src/components/shared/DataCards.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Card, CardBody, CardSubtitle, CardText, Badge } from "reactstrap";
 import { Colxx } from "components/common/Colxx";
+import NoImg from "assets/images/NoImg.jpg";
 const DataCards = ({ product }) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+  const [imgSrc, setImgSrc] = useState(
+    product.filename ? product.filename : NoImg
+  );
+
+  const onError = () => {
+    if (!imgLoaded) {
+      setImgSrc(NoImg);
+      setImgLoaded(true);
+    }
+  };
+
   return (
     <Colxx cla sm="6" lg="4" xl="3" className="mb-3 " key={product.id}>
       <Card className="card--shadow">
@@ -11,8 +24,11 @@ const DataCards = ({ product }) => {
               <Row className="justify-content-center">
                 <Colxx lg={8} md={6} sm={6} xs={6} xxs={8}>
                   <img
+                    className="image--default--background"
                     alt={product.name}
-                    src={product.filename}
+                    src={imgSrc}
+                    onError={() => onError()}
+                    loading="lazy"
                     width={"100%"}
                   />
                 </Colxx>
